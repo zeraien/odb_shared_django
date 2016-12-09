66e87e1ae8c3578b97559056e8d6820ec26b8604
Dmitri Fedortchenko <d@angelhill.net>
2016-12-09 01:51:19 +0100

    translations and time_helpers

--- templatetags/time_helpers.py
+++ templatetags/time_helpers.py
@@ -1,3 +1,4 @@
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 from django.template import Library
 from math import ceil, floor
@@ -5,11 +6,17 @@ from math import ceil, floor
 register = Library()
 
 @register.filter
+def time_from_minutes(total_minutes):
+    return time_from_seconds(total_minutes*60.)
+time_from_minutes.is_safe=True
+
+@register.filter
 def time_from_seconds(total_seconds):
-    if total_seconds is None or type(total_seconds) not in (int, long):
+    if total_seconds is None or type(total_seconds) not in (int, long, float):
         return ""
 
     hours, remainder = divmod(total_seconds, 3600)
+    hours = int(hours)
     minutes = int(floor(remainder // 60))
     seconds = int(round(remainder - (minutes * 60)))
 
@@ -20,8 +27,12 @@ def time_from_seconds(total_seconds):
     }
 
     if minutes == 0:
-        return _("%(seconds)ss") % timedata
+        return mark_safe(_("%(seconds)ss") % timedata)
     elif hours == 0:
-        return _("%(minutes)sm %(seconds)ss") % timedata
+        return mark_safe(_("%(minutes)sm %(seconds)ss") % timedata)
+    elif seconds == 0:
+        return mark_safe(_("%(hours)sh %(minutes)sm") % timedata)
     else:
-        return _("%(hours)sh %(minutes)sm %(seconds)ss") % timedata
+        return mark_safe(_("%(hours)sh %(minutes)sm %(seconds)ss") % timedata)
+
+time_from_seconds.is_safe=True

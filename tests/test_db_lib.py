a430ed99b31569b2cd50959c4f078d221f69c748
Dimo <d@angelhill.net>
2020-04-07 07:15:13 +0530

    test the database query generator

--- tests/test_db_lib.py
+++ tests/test_db_lib.py
@@ -20,30 +20,54 @@ class Test(TestCase):
         data_pairs = [("key", 1)]
         get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
 
+    def test_case_1(self):
         data_pairs = [
-            ("key","value")
+            ("keyaz","valueaz")
         ]
-        q = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
+        q,values = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
         self.assertEqual(
-            'SELECT D0.id_foo as `id` FROM table_bar as D0 WHERE (D0.key="key" AND D0.value IN ("value")) GROUP BY D0.id_foo ORDER BY D0.id_foo',
+            'SELECT D0.id_foo as `id` FROM table_bar as D0 WHERE (D0.key=%(key0)s AND D0.value IN (%(val0.0)s)) GROUP BY D0.id_foo ORDER BY D0.id_foo',
             q
         )
+        self.assertDictEqual(values, {'key0': "keyaz", 'val0.0': "valueaz"})
 
+    def test_case_2(self):
         data_pairs = [
             ("key",["value1","value2"])
         ]
-        q = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
+        q,values = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
         self.assertEqual(
-            'SELECT D0.id_foo as `id` FROM table_bar as D0 WHERE (D0.key="key" AND D0.value IN ("value1","value2")) GROUP BY D0.id_foo ORDER BY D0.id_foo',
+            'SELECT D0.id_foo as `id` FROM table_bar as D0 WHERE (D0.key=%(key0)s AND D0.value IN (%(val0.0)s,%(val0.1)s)) GROUP BY D0.id_foo ORDER BY D0.id_foo',
             q
         )
+        self.assertDictEqual(values, {'key0': "key", 'val0.0': "value1", 'val0.1': "value2"})
 
+    def test_case_3(self):
         data_pairs = [
-            ("key1",["value1","value2"]),
-            ("key2",["value2.1","value2.2"])
+            ("key1foobarvalue",["the_value1","the_value2"]),
+            ("key2bzainga",["the_value2.1","the_value2.2"])
         ]
-        q = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
+        q,values = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[])
         self.assertEqual(
-            'SELECT D0.id_foo as `id` FROM table_bar as D0  LEFT JOIN table_bar as D1 ON D0.id_foo = D1.id_foo WHERE (D0.key="key1" AND D0.value IN ("value1","value2")) AND (D1.key="key2" AND D1.value IN ("value2.1","value2.2")) GROUP BY D0.id_foo ORDER BY D0.id_foo',
+            'SELECT D0.id_foo as `id` FROM table_bar as D0  LEFT JOIN table_bar as D1 ON D0.id_foo = D1.id_foo WHERE (D0.key=%(key0)s AND D0.value IN (%(val0.0)s,%(val0.1)s)) AND (D1.key=%(key1)s AND D1.value IN (%(val1.0)s,%(val1.1)s)) GROUP BY D0.id_foo ORDER BY D0.id_foo',
             q
         )
+        self.assertDictEqual(values, {'key0': "key1foobarvalue",
+                                      'key1':'key2bzainga',
+                                      'val0.0': "the_value1",
+                                      'val0.1': "the_value2",
+                                      'val1.0': "the_value2.1",
+                                      'val1.1': "the_value2.2"})
+
+    def test_with_limit_to_ids(self):
+        data_pairs = [
+            ("k","v")
+        ]
+        q,values = get_query_for_data_pairs(data_pairs, "id_foo", "table_bar", limit_to_ids=[1,2,3])
+        self.assertEqual(
+            'SELECT D0.id_foo as `id` FROM table_bar as D0 WHERE D0.id_foo IN (%(id0)s,%(id1)s,%(id2)s) AND (D0.key=%(key0)s AND D0.value IN (%(val0.0)s)) GROUP BY D0.id_foo ORDER BY D0.id_foo',
+            q
+        )
+        self.assertDictEqual(values, {
+            'id0': 1, 'id1': 2, 'id2': 3,
+            'key0': "k",'val0.0': "v",})

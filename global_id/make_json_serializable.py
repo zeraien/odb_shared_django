48ab1fa939af54feffe1ac2e342d1da46fdeb52f
Dimo <d@angelhill.net>
2020-04-08 20:50:48 +0530

    add support to json to serialize dates and times

--- global_id/make_json_serializable.py
+++ global_id/make_json_serializable.py
@@ -2,15 +2,25 @@
 JSONEncoder.default() automatically checks for a special "to_json()"
 method and uses it to encode the object if found.
 """
+from datetime import datetime, date
 from decimal import Decimal
 from json import JSONEncoder
+
+from django.conf import settings
 from hashid_field import Hashid
+
+from odb_shared.time_helpers import to_epoch
+
 
 def _default(self, obj):
     if issubclass(obj.__class__, Hashid):
         return str(obj)
     elif isinstance(obj, Decimal):
         return float(obj)
+    elif isinstance(obj, datetime):
+        return to_epoch(obj)
+    elif isinstance(obj, date):
+        return obj.strftime(settings.PY_DATE_FORMAT)
 
     return _default.default(obj)
 

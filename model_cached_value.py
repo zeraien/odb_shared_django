0ce163c838d819c9faf5b02896c4cbe2e16c70cf
Dimo <d@angelhill.net>
2020-07-23 14:05:31 +0530

    respect method arguments when doing @cached_value

--- model_cached_value.py
+++ model_cached_value.py
@@ -1,3 +1,5 @@
+import hashlib
+import pickle
 from functools import wraps
 
 from django.core.cache import cache
@@ -8,7 +10,10 @@ def _get_key(instance):
 def cached_value(func):
     """
     returns the value of the decorated method, but checks the cache first.
-    Cache is based on the Django model instance of the decorated method.
+
+    Cache is based on the Django model instance of the decorated method,
+    it will respect method args and kwargs.
+
     To reset cache, call `reset_cache_for_instance`; reset should be called whenever changes
     to the underlying model or related children/parents are made.
 
@@ -18,9 +23,14 @@ def cached_value(func):
     """
     @wraps(func)
     def inner_func(self, *args, **kwargs):
-        value_key = func.__name__
         key = _get_key(self)
         data = cache.get(key,dict())
+
+        pickled_args = pickle.dumps(args)
+        pickled_kwargs = pickle.dumps(kwargs)
+        arg_hash = '%s%s' % (hashlib.md5(pickled_args).hexdigest(), hashlib.md5(pickled_kwargs).hexdigest())
+        value_key = '%s_%s' % (func.__name__, arg_hash)
+
         value = data.get(value_key)
         if value is None:
             value = data[value_key] = func(self, *args, **kwargs)
